edd9d1938a424c4d043692c11edb438439638dc1
Yahia Ibrahim <Yahiasheriif87@gmail.com>
2023-11-27 22:46:47 +0200

    New update

--- Gaussian-Jordan-Method/GaussianJordanElimination.cs
+++ Gaussian-Jordan-Method/GaussianJordanElimination.cs
@@ -81,6 +81,40 @@ namespace ConsoleApplication6
             sb.Append("Displaying input Matrix \n");
             sb.Append(matToString(a));
 
+            bool rowsAreScalarMultiple = true;
+            for (int i = 1; i < rows; i++)
+            {
+                double scalar = a[i, 0] / a[0, 0];
+                for (int j = 1; j < cols; j++)
+                {
+                    if (a[i, j] != scalar * a[0, j])
+                    {
+                        rowsAreScalarMultiple = false;
+                        break;
+                    }
+                }
+                if (!rowsAreScalarMultiple)
+                {
+                    break;
+                }
+            }
+
+            if (rowsAreScalarMultiple)
+            {
+                Console.WriteLine("The system has an infinite set of solutions.");
+                sb.Append("The system has an infinite set of solutions.\n");
+                return sb.ToString();
+            }
+
+
+
+            if (cols - 1 > rows)
+            {
+                Console.WriteLine("The system has an infinite set of solutions.");
+                sb.Append("The system has an infinite set of solutions.\n");
+                return sb.ToString();
+            }
+
             for (int i = 0; i < rows; i++)
             {
                 if (a[i, i] == 0)
@@ -91,19 +125,37 @@ namespace ConsoleApplication6
                         {
                             swapRows(i, k);
 
-                            Console.WriteLine("Swapping row " + i + " with row " + k);
-                            sb.Append("Swapping row " + i + " with row " + k + "\n");
+                            Console.WriteLine("Swapping row " + (i + 1) + " with row " + (k + 1));
+                            sb.Append("Swapping row " + (i + 1) + " with row " + (k + 1) + "\n");
 
                             break;
                         }
                     }
 
-                    if (a[i, i] == 0)
+                    bool allZeroesLastRow = true;
+                    for (int j = 0; j < cols - 1; j++)
                     {
-                        Console.WriteLine("Matrix is singular. Unable to continue.");
-                        sb.Append("Matrix is singular. Unable to continue.\n");
+                        if (a[rows - 1, j] != 0)
+                        {
+                            allZeroesLastRow = false;
+                            break;
+                        }
+                    }
+
+                    // Check if the last row has all zeros except the last element
+                    if (allZeroesLastRow && a[rows - 1, cols - 1] != 0)
+                    {
+                        Console.WriteLine("The system has no solution.");
+                        sb.Append("The system has no solution.\n");
                         return sb.ToString();
                     }
+
+                    //if (a[i, i] == 0)
+                    //{
+                    //    Console.WriteLine("Matrix is singular. Unable to continue.");
+                    //    sb.Append("Matrix is singular. Unable to continue.\n");
+                    //    return sb.ToString();
+                    //}
                 }
 
                 double temp = a[i, i];
@@ -113,9 +165,9 @@ namespace ConsoleApplication6
                     a[i, j] = a[i, j] / temp;
                 }
 
-                Console.WriteLine("Row operation is row" + i + " is divided by " + temp);
+                Console.WriteLine("R" + (i + 1) + " is divided by " + temp);
                 printMatrix(a);
-                sb.Append("Row operation is row" + i + " is divided by " + temp + " \n");
+                sb.Append("R" + (i + 1) + " is divided by " + temp + " \n");
                 sb.Append(matToString(a));
 
                 for (int k = 0; k < rows; k++)
@@ -127,11 +179,11 @@ namespace ConsoleApplication6
                         {
                             a[k, j] = a[k, j] - (temp * a[i, j]);
                         }
-                        Console.WriteLine("Row operation is row" + i + " is multiplied by " + temp +
-                                          " and then subtracted from row" + k);
+                        Console.WriteLine("R" + (i + 1) + " is multiplied by " + temp +
+                                          " and then subtracted from R" + (k + 1));
                         printMatrix(a);
-                        sb.Append("Row operation is row" + i + " is multiplied by " + temp +
-                                  " and then subtracted from row" + k + " \n");
+                        sb.Append("R" + (i + 1) + " is multiplied by " + temp +
+                                  " and then subtracted from R" + (k + 1) + " \n");
                         sb.Append(matToString(a));
                     }
                 }
@@ -142,42 +194,74 @@ namespace ConsoleApplication6
             sb.Append("Solved matrix \n");
             sb.Append(matToString(a));
 
+            //bool allZeroesLastRow = true;
+            //for (int j = 0; j < cols - 1; j++)
+            //{
+            //    if (a[rows - 1, j] != 0)
+            //    {
+            //        allZeroesLastRow = false;
+            //        break;
+            //    }
+            //}
+
+            //// Check if the last row has all zeros except the last element
+            //if (allZeroesLastRow && a[rows - 1, cols - 1] != 0)
+            //{
+            //    Console.WriteLine("The system has no solution.");
+            //    sb.Append("The system has no solution.\n");
+            //    return sb.ToString();
+            //}
+
+            // Extracting the values of x1, x2, ..., xn from the solved matrix
+            int numVariables = cols - 1;
+            double[] solution = new double[numVariables];
+
+            for (int i = 0; i < numVariables; i++)
+            {
+                solution[i] = a[i, cols - 1];
+                Console.WriteLine("x" + (i + 1) + " = " + solution[i]);
+                sb.Append("x" + (i + 1) + " = " + solution[i] + "\n");
+            }
+
             return sb.ToString();
         }
-
-        public static double DET(int n, double[,] Mat)
-        {
-            double d = 0;
-            int k, i, j, subi, subj;
-            double[,] SUBMat = new double[n, n];
-
-            if (n == 2)
-            {
-                return ((Mat[0, 0] * Mat[1, 1]) - (Mat[1, 0] * Mat[0, 1]));
-            }
-            else
-            {
-                for (k = 0; k < n; k++)
-                {
-                    subi = 0;
-                    for (i = 1; i < n; i++)
-                    {
-                        subj = 0;
-                        for (j = 0; j < n; j++)
-                        {
-                            if (j == k)
-                            {
-                                continue;
-                            }
-                            SUBMat[subi, subj] = Mat[i, j];
-                            subj++;
-                        }
-                        subi++;
-                    }
-                    d = d + (Math.Pow(-1, k) * Mat[0, k] * DET(n - 1, SUBMat));
-                }
-            }
-            return d;
-        }
     }
-}
+
+
+
+
+        //public static double DET(int n, double[,] Mat)
+        //{
+        //    double d = 0;
+        //    int k, i, j, subi, subj;
+        //    double[,] SUBMat = new double[n, n];
+
+        //    if (n == 2)
+        //    {
+        //        return ((Mat[0, 0] * Mat[1, 1]) - (Mat[1, 0] * Mat[0, 1]));
+        //    }
+        //    else
+        //    {
+        //        for (k = 0; k < n; k++)
+        //        {
+        //            subi = 0;
+        //            for (i = 1; i < n; i++)
+        //            {
+        //                subj = 0;
+        //                for (j = 0; j < n; j++)
+        //                {
+        //                    if (j == k)
+        //                    {
+        //                        continue;
+        //                    }
+        //                    SUBMat[subi, subj] = Mat[i, j];
+        //                    subj++;
+        //                }
+        //                subi++;
+        //            }
+        //            d = d + (Math.Pow(-1, k) * Mat[0, k] * DET(n - 1, SUBMat));
+        //        }
+        //    }
+        //    return d;
+        //}
+    }
--- Gaussian-Jordan-Method/MainGUI.cs
+++ Gaussian-Jordan-Method/MainGUI.cs
@@ -16,11 +16,12 @@ namespace ConsoleApplication6
     public partial class MainGUI : Form
     {
         GaussianJordanElimination gaus = new GaussianJordanElimination();
+        private int det;
+
         public MainGUI()
         {
             InitializeComponent();
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
-
         }
 
         private void MainGUI_Load(object sender, EventArgs e)
@@ -43,22 +44,20 @@ namespace ConsoleApplication6
 
         }
 
-        private DataTable createTable(int rows, int cols) {
+        private DataTable createTable(int rows, int cols)
+        {
             DataTable table = new DataTable();
             for (int x = 0; x < cols; x++)
             {
                 table.Columns.Add("X" + x, typeof(double));
-                
             }
             for (int y = 0; y < rows; y++)
             {
-                int[,] array = new int[1, cols];
-
-                table.Rows.Add(array.Cast<object>().ToArray());
+                table.Rows.Add(); // Add empty rows
             }
-
             return table;
         }
+
         private void button1_Click(object sender, EventArgs e)
         {
             int rows = 0;
@@ -77,7 +76,7 @@ namespace ConsoleApplication6
                 DataTable table = createTable(rows, cols);
                 dataGridView1.Visible = true;
                 dataGridView1.DataSource = table;
-                dataGridView1.Size = new Size((cols * 41) + 30, (rows * 25) + 15);
+                dataGridView1.Size = new Size((cols * 70) + 30, (rows * 25) + 15); // Adjusted column width
                 Console.WriteLine(rows + " " + cols);
             }
             catch (FormatException)
@@ -85,7 +84,6 @@ namespace ConsoleApplication6
                 MessageBox.Show("Rows and Columns must be an integer");
             }
         }
-
 
         private void button2_Click(object sender, EventArgs e)
         {
@@ -98,25 +96,47 @@ namespace ConsoleApplication6
             {
                 for (int x = 0; x < cols; x++)
                 {
-                    data[y, x] = double.Parse(table.Rows[y][x].ToString());
+                    if (!double.TryParse(table.Rows[y][x].ToString(), out data[y, x]))
+                    {
+                        MessageBox.Show("Invalid input at row " + (y + 1) + ", column " + (x + 1));
+                        return;
+                    }
                 }
             }
 
-            double det = GaussianJordanElimination.DET(rows, data);
+            //double det = GaussianJordanElimination.DET(rows, data);
 
-            if (det == 0)
-            {
-                richTextBox1.Text = "Can not be solved as determinant=0";
-            }
-            else
-            {
+            
+
+            
+            
                 gaus.setArray(data);
-                richTextBox1.Text = gaus.solve();
-            }
+                string solution = gaus.solve();
+
+                richTextBox1.Text = "Step 1: Create the Augmented Matrix\n";
+                richTextBox1.Text += "The augmented matrix for this system is:\n";
+                richTextBox1.Text += gaus.matToString(data) + "\n";
+
+                if (solution.Contains("infinite set of solutions"))
+                {
+                    richTextBox1.Text += "Step 2: Perform Gaussian Jordan Elimination\n";
+                    richTextBox1.Text += "\n";
+                    richTextBox1.Text += " the matrix transformations step by step.\n\n";
+                    richTextBox1.Text += "Step-by-Step Gaussian Jordan Elimination:\n";
+                    richTextBox1.Text += solution;
+                }
+                else
+                {
+                    richTextBox1.Text += "Solution:\n";
+                    richTextBox1.Text += solution;
+                    //richTextBox1.Text += "\nNow, let's determine the nature of the solution based on the structure of the augmented matrix to see if it has no solution or an infinite set of solutions.";
+                    richTextBox1.Text += "DONE";
+                }
+            
         }
 
 
-        private void panel2_Paint(object sender, PaintEventArgs e)
+                private void panel2_Paint(object sender, PaintEventArgs e)
         {
 
         }
